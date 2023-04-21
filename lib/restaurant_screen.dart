20724dd4c69a609a5682f64b32210d533b138dbc
Clarissa Gracienne <clarissagracia.cg@gmail.com>
2023-04-21 10:42:13 +0700

    Navbar

--- lib/restaurant_screen.dart
+++ lib/restaurant_screen.dart
@@ -14,6 +14,12 @@ class RestaurantsScreen extends StatefulWidget {
 }
 
 class _RestaurantsScreenState extends State<RestaurantsScreen> {
+   int _selectedIndex = 0;
+     void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,6 +37,25 @@ class _RestaurantsScreenState extends State<RestaurantsScreen> {
               },
             );
         } ,
+      ),
+       bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.explore),
+            label: 'Explore',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.login),
+            label: 'Login',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue[800],
+        onTap: _onItemTapped,
       ),
     );
   }
